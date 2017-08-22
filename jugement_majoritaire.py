50ff0a19cdf21b63208742a18e8e40fcf79a28c7
Florian <florian.lescuyer@gmail.com>
2017-08-22 15:24:38 +0200

    Cration of file jugement_majoritaire.py

---
+++ jugement_majoritaire.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import random
+
+# Initialize seed so we always get the same result between two runs.
+# Comment this out if you want to change results between two runs.
+# More on this here: http://stackoverflow.com/questions/22639587/random-seed-what-does-it-do
+random.seed(0)
+
+##################################################
+#################### VOTES SETUP #################
+##################################################
+
+VOTES = 100000
+MEDIAN = VOTES/2
+CANDIDATES = {
+    "hermione": "Hermione Granger",
+    "balou": "Balou",
+    "chuck-norris": "Chuck Norris",
+    "elsa": "Elsa",
+    "gandalf": "Gandalf",
+    "beyonce": "Beyoncé"
+}
+
+MENTIONS = [
+    "A rejeter",
+    "Insuffisant",
+    "Passable",
+    "Assez Bien",
+    "Bien",
+    "Très bien",
+    "Excellent"
+]
+
+def create_votes():
+    return [
+        {
+            "hermione": random.randint(3, 6),
+            "balou": random.randint(0, 6),
+            "chuck-norris": random.randint(0, 2),
+            "elsa": random.randint(1, 2),
+            "gandalf": random.randint(3, 6),
+            "beyonce": random.randint(2, 6)
+        } for _ in range(0, VOTES)
+    ]
+
+##################################################
+#################### FUNCTIONS ###################
+##################################################
+
+# Your code goes here
+
+
+##################################################
+#################### MAIN FUNCTION ###############
+##################################################
+
+def main():
+    votes = create_votes()
+
+if __name__ == '__main__':
+main()
